13416ce267169e65954463cb8cb16e2665cc20b9
xx0hcd <xx0hcd@gmail.com>
2018-05-03 14:53:19 -0500

    cleaned up script.
    removed some manual config parts.

--- portknocker.py
+++ portknocker.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python
 
-##Uncomment this if you want to randomize ports in the list..
+##Uncomment this and 'random.shuffle(ports)' if you want to randomize ports in the list.
 #import random
 
-##Just using to keep track of attempts in print statements.
-count = 0
- 
+counts = 0
+
+ipaddr = raw_input("Enter destination IP address: ")
+
 from scapy.all import *
 conf.verb = 0
 
-##The List of ports to try in order...
-ports = [1, 2, 3]
+port_in = raw_input("Enter ports to use comma separated. (i.e. 7000,666,8890) : ")
+ports = [int(i) for i in port_in.split(',')]
+print "Knocking with ports :", ports, "on host ", ipaddr
 
-##... or uncomment this to randomize ports in your list.
+#uncomment this and 'import random' to randomize ports in the list.
 #random.shuffle(ports)
 
 for dport in range(0, len(ports)):
- 
-    ##Just using to keep track of attempts in print statements.
-    count = count+1
-    ##Destination IP   
-    ip = IP(dst="192.168.34.133")
+    count = counts+1
+    ip = IP(dst=ipaddr)
     ##Source port you want to use
     port = 35001
     SYN = ip/TCP(sport=port, dport=ports[dport], flags="S", window=2048, options=[('MSS',1460)], seq=0)
-    send(SYN) ; print "Knock ---> #", count
+    send(SYN)
 
-    ##Add more lines here if you want to hit the list multiple times. i.e. 1,1,2,2,3,3,etc.
-    
-print '-------------'
-print '-------------'
-print 'Start nmap  :'
-print '-------------'
-print '-------------'
-subprocess.call("nmap -sV -T4 192.168.34.133", shell=True)
-print '-------------'
-print '-------------'
-print 'Finish nmap :'
-print '-------------'
-print '-------------'
+for x in ports:
+    print '------------------------------------------'
+    print "Knock ---> port#", x
+
+  
+print '------------------------------------------'
+print '------------------------------------------'
+print 'Running nmap to check for new open ports: '
+print '------------------------------------------'
+print '------------------------------------------'
+#run nmap to check what new ports are open.
+nvar = "nmap -p- -T4 --reason " + ipaddr
+subprocess.call(nvar, shell=True)
+print '------------------------------------------'
+print '------------------------------------------'
+print 'Finished running nmap. '
+print '------------------------------------------'
+print '-----------------------'
